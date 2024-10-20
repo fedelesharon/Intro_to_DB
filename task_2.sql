f37cdf822ecdb421cf057c06fe05522e7ac6869d
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-10-20 11:28:03 +0300

    sql

--- task_2.sql
+++ task_2.sql
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS Authors (
 -- Create customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
@@ -43,6 +43,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
 
 
 
