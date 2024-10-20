b2214ae18e4035e7bfb48959a7ad107508ec8de1
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-10-20 10:58:21 +0300

    sql

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Query to print the full description of the books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'books'
+AND TABLE_SCHEMA = 'alx_book_store';
