163dcd9ef60e118a3cc3e698597df2442e558cab
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2024-10-20 11:04:11 +0300

    sql

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Select the alx_book_store database
+USE alx_book_store;
+
+-- Insert a single row into the customer table
+INSERT INTO customers (customer_id, first_name, last_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
